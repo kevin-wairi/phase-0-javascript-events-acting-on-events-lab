a7335c17750d23c22dec3ea38f8bc3f2cda72531
kevin Wairi <kevinwairi@gmail.com>
2023-03-24 07:46:42 +0300

    Adds right limit for dodger inside the game

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 
 document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
+    if (e.key === "ArrowUp") {
       moveDodgerLeft();
     }
   });
@@ -28,7 +28,7 @@ function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
-    if (left > 0) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
